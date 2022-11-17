b75ba7e30f69fe8d86485ebae22d46bb698d08cb
fernandaop <fernandaop@al.insper.edu.br>
2022-11-16 21:55:37 -0300

    com

--- vocab-frontend/src/App.js
+++ vocab-frontend/src/App.js
@@ -2,6 +2,7 @@ import Vocab from "./components/Vocab";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Words from "./components/Words";
 
 function App() {
   const [vocabs, setVocabs] = useState([]); // Remova o array de notes que existia na versão anterior
@@ -12,7 +13,21 @@ function App() {
       .then((res) => setVocabs(res.data));
   }, []);
   
-
+  var palavra = vocabs;
+  const comp = 0;
+  console.log(palavra);
+  //comparar palavra com o que o usuario digitou letra por letra
+  //se for igual, mostrar a palavra
+  //se for diferente, mostrar a palavra com a letra errada em vermelho
+  //se for igual, mostrar a palavra com a letra correta em verde
+  function compareWords(palavrausuario){
+    for (let i = 0; i < palavra.length; i++) {
+      if (palavra[i] === palavrausuario[i]) {
+        comp = 1;
+      } else {
+        comp= -1;}
+      return comp;
+  }
   return (
     <div className="App">
       <div className="appbar">
@@ -21,9 +36,11 @@ function App() {
       <h1>
         <span className="titlev">VOCAB</span>
       </h1>
-      <Vocab title={vocabs.title}>{vocabs.content}</Vocab>
+      <Vocab>{vocabs.title}</Vocab>
+      <Words></Words>
     </div>
   );
+  }
 }
 
 export default App;
