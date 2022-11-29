758975b37311d9e6af461a8119feb71146bb32ec
Guilherme Martins <guilhermesm9@al.insper.edu.br>
2022-11-29 09:28:46 -0300

    autenticação

--- vocab-frontend/src/components/Autenticacao/index.js
+++ vocab-frontend/src/components/Autenticacao/index.js
@@ -9,26 +9,17 @@ export default function Autenticacao(props) {
     // const options = { headers: {'Content-Type':'application/json', 'Accept':'application/json'} };
     // const userEmail = user + "@"
 
-    function enviarInfo() {
-        // USANDO ESSA FUNCAO POR ENQUANTO PORQUE O BACKEND AINDA NAO FOI ATUALIZADO
-        console.log("enviou");
-    }
 
-    // const enviarInfo = (event) => {
-    //     event.preventDefault();
-    //     axios
-    //       .post('http://127.0.0.1:8000/api/palavras/token',{username: user, password: password, email: userEmail}, options)
-    //       .then((response) => {
-    //         const acesso = 'Token ' + response.data.accessToken;
-    //         const options2 = { headers: {'Content-Type':'application/json', 'Accept':'application/json', 'Authorization':acesso} };
-    //         axios.get('http://127.0.0.1:8000/api/palavras/', options2);
-    //         // response.data["user"] = user;
-    //         // response.data["password"] = password;
-    //         // navigate("/procurar", { state: response.data })
-    //         setUser("");
-    //         setPassword("");
-    //       });
-    //   }
+    function enviarInfo(event){
+        event.preventDefault();
+        axios
+          .post('http://127.0.0.1:8000/api/palavras/token',{username: user, password: password}, options)
+          .then((response) => {
+            const acesso = 'Token ' + response.data.accessToken;
+            const options2 = { headers: {'Content-Type':'application/json', 'Accept':'application/json', 'Authorization':acesso} };
+            return acesso;
+          });
+      }
 
     const userChanged = (event) => { setUser(event.target.value); }
     const passwordChanged = (event) => { setPassword(event.target.value); }
